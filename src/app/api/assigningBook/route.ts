1cab99fa4ed6d70fd87c76a8b6b547e37d24c677
rezwanMahadi <rezwanam2@gmail.com>
2025-09-28 18:04:45 +0600

    m

--- src/app/api/assigningBook/route.ts
+++ src/app/api/assigningBook/route.ts
@@ -3,16 +3,34 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
     const { universityId, bookId } = await request.json();
+    const findBook = await prisma.orderedBook.findUnique({
+        where: {
+            bookId: bookId
+        }
+    })
+    if (findBook) {
+        const deleteBook = await prisma.orderedBook.delete({
+            where: {
+                bookId: bookId
+            }
+        })
+        if (deleteBook){
+            return NextResponse.json({ success: true }, { status: 200 });
+        }
+        else {
+            return NextResponse.json({ success: false }, { status: 400 });
+        }
+    }
     const assignBooks = await prisma.orderedBook.create({
         data: {
             universityId: universityId,
             bookId: bookId
         }
     });
-    if (assignBooks){
-        return NextResponse.json({success: true}, {status: 200});
+    if (assignBooks) {
+        return NextResponse.json({ success: true }, { status: 200 });
     }
-    else{
-        return NextResponse.json({success: false}, {status: 400});
+    else {
+        return NextResponse.json({ success: false }, { status: 400 });
     }
 }
